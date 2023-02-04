e91e321676b7dcf2eed565d48c5ca1be6f9ce441
Filipe Almeida <filipesilva504@gmail.com>
2023-02-03 22:39:08 -0300

    some fixes

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,9 @@
-import SideBar from "../../components/SideBar"
 import * as S from './Home.style'
 
 const Home = (): JSX.Element => {
     return (
     <S.Container>
-        <SideBar />
+        oi
     </S.Container>
     )
 }
--- src/pages/Users/Users.tsx
+++ src/pages/Users/Users.tsx
@@ -1,10 +1,8 @@
-import SideBar from '../../components/SideBar'
 import * as S from './Users.style'
 
 const Home = (): JSX.Element => {
     return (
     <S.Container>
-        <SideBar /> 
         <h1>oioioiioioioipoi</h1>
     </S.Container>
     )
